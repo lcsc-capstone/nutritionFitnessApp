36e6e3e3dce1a3cb8c344781a32dc11637fa7f8a
ctmessan <ctmessan@gmail.com>
2019-01-29 22:07:26 -0800

    create connection file
    Contains basic connection to our database. To be added to main forms.

---
+++ src/app/connect.php
@@ -0,0 +1,22 @@
+<?php
+    
+    $servername = "bonsai.lcsc.edu";
+    $username = "ctmessan";
+    $password = "Fr0gTr!p#1528";
+    $dbname = "ctmessan_nutrition_app";
+
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+$sql = '!!!!!!!!INSERT QUERRY HERE BETWEEN SINGLE QUOTES!!!!!!!';
+		
+$query = mysqli_query($conn, $sql);
+
+if (!$query) {
+	die ('SQL Error: ' . mysqli_error($conn));
+}
+?>
