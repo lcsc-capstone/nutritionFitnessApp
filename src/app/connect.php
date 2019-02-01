1694deec38742bc4cdc0142ab4fa288fac9d3c2b
ctmessan <ctmessan@gmail.com>
2019-02-01 11:44:13 -0800

    Update connect.php

--- src/app/connect.php
+++ src/app/connect.php
@@ -1,5 +1,8 @@
 <?php
     
+    /*URL to add to 'action' property of form so it connects to database: 
+        bonsai.lcsc.edu/ctmessan/nutritionFitnessApp/src/app/connect.php*/
+   
     $servername = "bonsai.lcsc.edu";
     $username = "ctmessan";
     $password = "Fr0gTr!p#1528";
@@ -12,11 +15,11 @@
         die("Connection failed: " . $conn->connect_error);
     } 
 
-$sql = '!!!!!!!!INSERT QUERRY HERE BETWEEN SINGLE QUOTES!!!!!!!';
+/*$sql = '!!!!!!!!INSERT QUERRY HERE BETWEEN SINGLE QUOTES!!!!!!!';
 		
 $query = mysqli_query($conn, $sql);
 
 if (!$query) {
 	die ('SQL Error: ' . mysqli_error($conn));
-}
+}*/
 ?>
