16d7bbec63601c233153a249323a6cfb4a836e0e
BrandonAPotter <39147278+BrandonAPotter@users.noreply.github.com>
2019-02-13 11:38:59 -0800

    profile page

--- NutriBien/src/app/tabs/tabs.router.module.ts
+++ NutriBien/src/app/tabs/tabs.router.module.ts
@@ -44,6 +44,15 @@ const routes: Routes = [
         ]
       },
       {
+        path: 'profile',
+        children: [
+          {
+            path: '',
+            loadChildren: '../profile/profile.module#ProfilePageModule'
+          }
+        ]
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
