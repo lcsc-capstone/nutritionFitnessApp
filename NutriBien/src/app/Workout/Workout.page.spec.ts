74c17045e9215d7c1c751ccbabd6e48344e0e178
Bego <begonaandresm@gmail.com>
2019-03-06 11:10:36 -0800

    change tab name

--- NutriBien/src/app/Workout/Workout.page.spec.ts
+++ NutriBien/src/app/Workout/Workout.page.spec.ts
@@ -1,21 +1,21 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Tab2Page } from './tab2.page';
+import { WorkoutPage } from './Workout.page';
 
-describe('Tab2Page', () => {
-  let component: Tab2Page;
-  let fixture: ComponentFixture<Tab2Page>;
+describe('WorkoutPage', () => {
+  let component: WorkoutPage;
+  let fixture: ComponentFixture<WorkoutPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [Tab2Page],
+      declarations: [WorkoutPage],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(Tab2Page);
+    fixture = TestBed.createComponent(WorkoutPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
