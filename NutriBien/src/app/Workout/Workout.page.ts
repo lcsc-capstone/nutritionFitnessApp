0572f3518d4f0925905de7b1299fa568f36e2beb
Bego <begonaandresm@gmail.com>
2019-03-06 11:20:44 -0800

    more tab name changes

--- NutriBien/src/app/Workout/Workout.page.ts
+++ NutriBien/src/app/Workout/Workout.page.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  templateUrl: 'Workout.page.html',
+  styleUrls: ['Workout.page.scss']
 })
-export class Tab2Page {
+export class WorkoutPage {
   public sport:string 
   public showDistance:boolean=false
   changeSport(){  
--- NutriBien/src/app/tabs/tabs.router.module.ts
+++ NutriBien/src/app/tabs/tabs.router.module.ts
@@ -17,11 +17,11 @@ const routes: Routes = [
         ]
       },
       {
-        path: 'tab2',
+        path: 'Workout',
         children: [
           {
             path: '',
-            loadChildren: '../tab2/tab2.module#Tab2PageModule'
+            loadChildren: '../Workout/Workout.module#WorkoutPageModule'
           }
         ]
       },
