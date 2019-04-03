951b15174ce95635402ce52e79cb918b41b258b7
Yuka-K <ykatsuki@lcmail.lcsc.edu>
2019-04-03 10:40:58 -0700

    Update nutrition.page.ts

--- NutriBien/src/app/nutrition/nutrition.page.ts
+++ NutriBien/src/app/nutrition/nutrition.page.ts
@@ -14,53 +14,47 @@ import * as mongodb from 'mongodb';
 
 export class NutritionPage{
    
-  constructor(private formBuilder: FormBuilder)
-    {
-      this.nutrition = this.formBuilder.group(
-      {
-        //ADD PROMT TEXT FOR ALL VALIDATORS//
-        Proteins: new FormControl('Proteins', Validators.compose([
-          Validators.required,
-          NutrientsValidator.isValid
-        ])),
-        Carbs: new FormControl('Carbs', Validators.compose([
-          Validators.required,
-          NutrientsValidator.isValid
-        ])),
-        Fats: new FormControl('Fats', Validators.compose([
-          Validators.required,
-          NutrientsValidator.isValid
-        ])),
-        Fibers: new FormControl('Fibers', Validators.compose([
-          Validators.required,
-          NutrientsValidator.isValid
-        ])),
-        Calories: new FormControl('Calories', Validators.compose([
-          Validators.required,
-          NutrientsValidator.isValid
-        ]))
-      });
-    }
+  constructor(private formBuilder: FormBuilder){
+    this.nutrition = this.formBuilder.group({
+      Proteins: new FormControl('Proteins', Validators.compose([
+        Validators.required,
+        NutrientsValidator.isValid
+      ])),
+      Carbs: new FormControl('Carbs', Validators.compose([
+        Validators.required,
+        NutrientsValidator.isValid
+      ])),
+      Fats: new FormControl('Fats', Validators.compose([
+        Validators.required,
+        NutrientsValidator.isValid
+      ])),
+      Fibers: new FormControl('Fibers', Validators.compose([
+        Validators.required,
+        NutrientsValidator.isValid
+      ])),
+      Calories: new FormControl('Calories', Validators.compose([
+        Validators.required,
+        NutrientsValidator.isValid
+      ]))
+    });
+  }
 
-    public Proteins: number;
-    public Carbs: number;
-    public Fats: number;
-    public Fibers: number;
-    public Calories: number;
-    private nutrition : FormGroup;
+  public Proteins: number;
+  public Carbs: number;
+  public Fats: number;
+  public Fibers: number;
+  public Calories: number;
+  private nutrition : FormGroup;
 
-    /*public submit(): void 
-  {
-  
-
-    //const MongoClient = require('mongodb').MongoClient;
-    const uri = "mongodb+srv://nutri:<bien>@nutrition-fitness-app-dsodq.gcp.mongodb.net/admin?retryWrites=true";
+ public submit(): void {
+    
+    //const uri = "mongodb+srv://nutri:<bien>@nutrition-fitness-app-dsodq.gcp.mongodb.net/admin?retryWrites=true";
     const client = new mongodb.MongoClient(uri, { useNewUrlParser: true });
     client.connect(async(_err: any) => {
-      //const nutri = client.db("nutritionFitnessApp");
-      //console.log("connected to Mongo, whohoo!");
-      // perform actions on the collection object
-      /*await nutri.collection('NUTRITION').insertOne({
+      const nutri = client.db("nutritionFitnessApp");
+      console.log("connected to Mongo, whohoo!");
+      //perform actions on the collection object
+      await nutri.collection('NUTRITION').insertOne({
         PROTEINS: this.Proteins,
         CARBS: this.Carbs,
         FIBERS: this.Fibers
@@ -71,9 +65,8 @@ export class NutritionPage{
     // TESTING THE VARIABLE CONTAIN CORRECT VALUES 
     // console.log(this.Proteins);
     // console.log(this.Fibers); 
-  }*/
+  }
   
   //ngOnInit() {}
     
 }
-
