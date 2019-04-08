ad99cf83c84353628e337ba2d7735dc321f88593
ctmessan <46732268+ctmessan@users.noreply.github.com>
2019-04-07 20:03:24 -0700

    c

--- database.ts
+++ database.ts
@@ -14,66 +14,69 @@ client.on("error", function(err)
 });*/ 
 
 client.once("open", function(){
-    console.log("database connection successful");
+    console.log("database connection successful");});
 
-        const Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-    var ProfileSchema = new Schema
-    ({
-        ID_NUM: {type: Number},
-        LASTNAME: {type: String},
-        FIRSTNAME: {type: String},
-        PHONE: {type: Number},
-        EMAIL: {type: String}, 
-        PASSWORD: {type: String},
-        DATE_OF_BIRTH: {type: Date},
-        FEET: {type: Number},
-        INCHES: {type: Number}
-    });
-
-    var ExercixeSchema = new Schema
-    ({
-        ID_NUM: {type: Number},
-        TIME: {type: Number},
-        TYPE: {type: String},
-        DISTANCE: {type: Number}
-    });
-
-    var MeasurementsSchema = new Schema
-    ({
-        ID_NUM: {type: Number},
-        NECK: {type: Number},
-        HIPS: {type: Number},
-        THIGHS: {type: Number},
-        BELLY: {type: Number},
-        BICEP: {type: Number}
-    });
-
-    var NutritionSchema = new Schema
-    ({
-        ID_NUM: {type: Number},
-        PROTEINS: {type: Number},
-        CARBS: {type: Number},
-        FATS: {type: Number},
-        FIBERS: {type: Number},
-        CALORIES: {type: Number}
-    });
-
-    var StatisticsSchema = new Schema
-    ({
-        ID_NUM: {type: Number},
-        DATE: {type: Date},
-        WEIGHT: {type: Number},
-        WEEKLY_AVERAGE: {type: Number},
-        DIFFERENCE_TO_PRIOR_WEEK: {type: Number},
-        QUESTIONS: {type: String},
-    });
-
-
-    var PROFILE = mongoose.model('PROFILE', ProfileSchema);
-    var EXERCISE = mongoose.model('EXERCISE', ExercixeSchema);
-    var MEASUREMENTS = mongoose.model('MEASUREMENTS', MeasurementsSchema);
-    var NUTRITION = mongoose.model('NUTRITION', NutritionSchema);
-    var STATISTICS = mongoose.model('STATISTICS', StatisticsSchema);
-
+var ProfileSchema = new Schema
+({
+    ID_NUM: {type: Number},
+    LASTNAME: {type: String},
+    FIRSTNAME: {type: String},
+    PHONE: {type: Number},
+    EMAIL: {type: String}, 
+    PASSWORD: {type: String},
+    DATE_OF_BIRTH: {type: Date},
+    FEET: {type: Number},
+    INCHES: {type: Number}
 });
+
+var ExercixeSchema = new Schema
+({
+    ID_NUM: {type: Number},
+    TIME: {type: Number},
+    TYPE: {type: String},
+    DISTANCE: {type: Number}
+});
+
+var MeasurementsSchema = new Schema
+({
+    ID_NUM: {type: Number},
+    NECK: {type: Number},
+    HIPS: {type: Number},
+    THIGHS: {type: Number},
+    BELLY: {type: Number},
+    BICEP: {type: Number}
+});
+
+var NutritionSchema = new Schema
+({
+    ID_NUM: {type: Number},
+    PROTEINS: {type: Number},
+    CARBS: {type: Number},
+    FATS: {type: Number},
+    FIBERS: {type: Number},
+    CALORIES: {type: Number}
+});
+
+var StatisticsSchema = new Schema
+({
+    ID_NUM: {type: Number},
+    DATE: {type: Date},
+    WEIGHT: {type: Number},
+    WEEKLY_AVERAGE: {type: Number},
+    DIFFERENCE_TO_PRIOR_WEEK: {type: Number},
+    QUESTIONS: {type: String},
+});
+
+
+
+
+const PROFILE = mongoose.model('PROFILE', ProfileSchema);
+const EXERCISE = mongoose.model('EXERCISE', ExercixeSchema);
+const MEASUREMENTS = mongoose.model('MEASUREMENTS', MeasurementsSchema);
+const NUTRITION = mongoose.model('NUTRITION', NutritionSchema);
+const STATISTICS = mongoose.model('STATISTICS', StatisticsSchema);
+
+export default { PROFILE, EXERCISE, MEASUREMENTS, NUTRITION, STATISTICS}  ;
+
