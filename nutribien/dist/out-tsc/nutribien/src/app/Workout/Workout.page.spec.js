db003ab796b31532fa309a76615224eeee0e1904
ctmessan <46732268+ctmessan@users.noreply.github.com>
2019-04-11 09:59:23 -0700

    things

---
+++ nutribien/dist/out-tsc/nutribien/src/app/Workout/Workout.page.spec.js
@@ -0,0 +1,22 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, TestBed } from '@angular/core/testing';
+import { WorkoutPage } from './Workout.page';
+describe('WorkoutPage', function () {
+    var component;
+    var fixture;
+    beforeEach(async(function () {
+        TestBed.configureTestingModule({
+            declarations: [WorkoutPage],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+        }).compileComponents();
+    }));
+    beforeEach(function () {
+        fixture = TestBed.createComponent(WorkoutPage);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+    it('should create', function () {
+        expect(component).toBeTruthy();
+    });
+});
+//# sourceMappingURL=Workout.page.spec.js.map
