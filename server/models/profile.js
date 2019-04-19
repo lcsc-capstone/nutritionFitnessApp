d637736c1aceba93539a0d9137ac66cee542e14e
ctmessan <46732268+ctmessan@users.noreply.github.com>
2019-04-19 11:30:49 -0700

    Revert "Merge branch 'lastBranchOMGISwear' of https://github.com/lcsc-capstone/nutritionFitnessApp into lastBranchOMGISwear"
    This reverts commit a0835c584a7a2fe4c7728002923aa3f6e0628744, reversing
    changes made to 6bb836a9fbdc544f46ce46d48fd9a31beb8ffa99.

--- server/models/profile.js
+++ server/models/profile.js
@@ -6,13 +6,13 @@ var Schema              =   mongoose.Schema;
 ProfileSchema = new Schema
 ({
     ID_NUM          : {type: Number, required : true},
-    FIRSTNAME       : {type: String, required : true},
     LASTNAME        : {type: String, required : true},
+    FIRSTNAME       : {type: String, required : true},
     PHONE           : {type: Number, required : true},
     EMAIL           : {type: String, required : true}, 
     PASSWORD        : {type: String, required : true},
     DATE_OF_BIRTH   : {type: Date, required : true},
-    HEIGHT              : {type: Number, required : true}
+    CM              : {type: Number, required : true}
 });
 
 /* Export model for application usage */
