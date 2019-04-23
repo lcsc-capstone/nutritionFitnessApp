4f993912af4e95df96c634adcbb1e09815d98359
ctmessan <46732268+ctmessan@users.noreply.github.com>
2019-04-23 14:08:47 -0700

    app runs!

--- nutribien/src/app/app-routing.module.ts
+++ nutribien/src/app/app-routing.module.ts
@@ -5,12 +5,12 @@ const routes: Routes = [
   {path: '',redirectTo: 'home',pathMatch: 'full'},
   {path: 'home', loadChildren: './home/home.module#HomePageModule'},
   {path: 'list',loadChildren: './list/list.module#ListPageModule'},
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'nutrition', loadChildren: './nutrition/nutrition.module#NutritionPageModule'},
-  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
-  { path: 'workout', loadChildren: './Workout/Workout.module#WorkoutPageModule' },
-  { path: 'measurements', loadChildren: './Measurements/Measurements.module#MeasurementsPageModule' },
-  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' }
+  {path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  {path: 'nutrition', loadChildren: './nutrition/nutrition.module#NutritionPageModule'},
+  {path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  {path: 'workout', loadChildren: './Workout/Workout.module#WorkoutPageModule' },
+  {path: 'measurements', loadChildren: './Measurements/Measurements.module#MeasurementsPageModule' },
+  {path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' }
 ];
 
 @NgModule({
