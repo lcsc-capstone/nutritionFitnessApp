61fe0cb116d99957a197ec17532d9fb1e2318465
Yuka-K <ykatsuki@lcmail.lcsc.edu>
2019-04-22 21:19:38 -0700

    a

--- server/models/profile.js
+++ server/models/profile.js
@@ -12,7 +12,11 @@ ProfileSchema = new Schema
     EMAIL           : {type: String, required : true}, 
     PASSWORD        : {type: String, required : true},
     DATE_OF_BIRTH   : {type: Date, required : true},
+<<<<<<< HEAD
     HEIGHT          : {type: Number, required : true}
+=======
+    HEIGHT              : {type: Number, required : true}
+>>>>>>> parent of d637736... Revert "Merge branch 'lastBranchOMGISwear' of https://github.com/lcsc-capstone/nutritionFitnessApp into lastBranchOMGISwear"
 });
 
 /* Export model for application usage */
