e146d16a515827462a5e61608761e697bb4f6222
Yuka-K <ykatsuki@lcmail.lcsc.edu>
2019-04-26 03:39:39 -0700

    almost finished my part

--- nutribien/src/app/profile/profile.page.ts
+++ nutribien/src/app/profile/profile.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
@@ -9,11 +9,23 @@ import {ActivatedRoute} from "@angular/router";
 export class ProfilePage implements OnInit {
    public fName: string;
 
-   public constructor(private route: ActivatedRoute) {
+   public constructor(
+     private route: ActivatedRoute,
+    private router: Router) {
       this.route.queryParams.subscribe(params => {
           this.fName = params["fname"] ;
       });
   }
+  workout(){
+    this.router.navigate(['workout']);
+  }
+  measurements(){
+    this.router.navigate(['measurements']);
+  }
+  nutrition(){
+    this.router.navigate(['nutrition']);
+  }
+
  
 
   ngOnInit() {
