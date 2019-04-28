678e6dd4b82497563718983356468f294f8ec8a9
tromixx <veneret@gmail.com>
2019-04-27 18:31:26 -0700

    poter
    poter

---
+++ nutribien/src/app/muser/muser.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { MuserPage } from './muser.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MuserPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [MuserPage]
+})
+export class MuserPageModule {}
